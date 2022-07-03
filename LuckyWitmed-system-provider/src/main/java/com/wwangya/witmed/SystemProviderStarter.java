990162a6fcc040f3a573b78af1a301ab9eb77620
wwangya <19956572314@163.com>
2022-07-03 19:59:24 +0800

    LuckyWitmed

---
+++ LuckyWitmed-system-provider/src/main/java/com/wwangya/witmed/SystemProviderStarter.java
@@ -0,0 +1,21 @@
+package com.wwangya.witmed;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * <b>SystemProvider启动类</b>
+ *
+ * @author WWY
+ * 2022/7/3
+ */
+@EnableEurekaClient
+@SpringBootApplication
+@MapperScan("com.wwangya.witmed.dao")
+public class SystemProviderStarter {
+	public static void main(String[] args) {
+		SpringApplication.run(SystemProviderStarter.class, args);
+	}
+}
